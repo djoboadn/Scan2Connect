03ea72ef3745b4e74c3baf4d0d5d9b0df01fc2fb
djoboadn <djoboadn@hs-albsig.de>
2023-11-15 08:37:49 +0100

    started with splash activity

---
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.myapplication.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
