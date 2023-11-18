eb70a1dce9acb642225a43aa9e67f3ebffdce0d0
Jarne <dev@jarne.rocks>
2023-11-18 20:08:01 +0100

    Add library for generating QR codes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.android.material:material:1.2.1")
+    implementation("io.github.g0dkar:qrcode-kotlin:4.0.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
