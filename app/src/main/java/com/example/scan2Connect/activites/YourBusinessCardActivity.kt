b8e74618ebd8c0df2c85b4e6fdda5b1f50e01d5e
djoboadn <djoboadn@hs-albsig.de>
2024-01-01 09:01:23 +0100

    removed unnecessary import

--- app/src/main/java/com/example/scan2Connect/activites/YourBusinessCardActivity.kt
+++ app/src/main/java/com/example/scan2Connect/activites/YourBusinessCardActivity.kt
@@ -6,7 +6,6 @@ import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
